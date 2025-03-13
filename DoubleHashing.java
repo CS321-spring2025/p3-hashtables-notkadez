3e24860fbfad005c0e569b5b08e6ead14531e8c5
Kayden Humphries <kaydenhump4905@gmail.com>
2025-03-12 18:57:25 -0600

    Implemented DoubleHashing and LinearProbing using the abstract HashTable class

---
+++ DoubleHashing.java
@@ -0,0 +1,42 @@
+/**
+ * DoubleHashing.java
+ *
+ * Subclass of Hashtable implementing double hashing
+ *
+ * @author Kayden Humphries
+ */
+public class DoubleHashing extends Hashtable {
+
+    /**
+     * Constructor to initialize the hash table with a given capacity
+     *
+     * @param capacity the capacity of the hash table
+     */
+    public DoubleHashing(int capacity) {
+        super(capacity);
+    }
+
+    /**
+     * Primary hash function for double hashing
+     * h1(k) = k mod m, where k is the key and m is size of table
+     *
+     * @param key the key to hash
+     * @return the hash value of the key
+     */
+    @Override
+    public int h1(Object key) {
+        return positiveMod(key.hashCode(), size);
+    }
+
+    /**
+     * Secondary hash function for double hashing
+     * h2(k) = 1 + k mod (m - 2), where k is the key and m is size of table
+     *
+     * @param key the key to hash
+     * @return the secondary hash value
+     */
+    @Override
+    public int h2(Object key) {
+        return 1 + positiveMod(key.hashCode(), size - 2);
+    }
+}
---
+++ LinearProbing.java
@@ -0,0 +1,41 @@
+/**
+ * LinearProbing.java
+ *
+ * Subclass of Hashtable implementing linear probing
+ *
+ * @author Kayden Humphries
+ */
+public class LinearProbing extends Hashtable {
+
+    /**
+     * Constructor to initialize the hash table with a given capacity
+     *
+     * @param capacity the capacity of the hash table
+     */
+    public LinearProbing(int capacity) {
+        super(capacity);
+    }
+
+    /**
+     * Primary hash function for linear probing
+     * h1(k) = k mod m, where k is the key and m is size of table
+     *
+     * @param key the key to hash
+     * @return the hash value of the key
+     */
+    @Override
+    public int h1(Object key) {
+        return positiveMod(key.hashCode(), size);
+    }
+
+    /**
+     * Secondary hash function for linear probing is 1
+     *
+     * @param key the key to hash
+     * @return 1
+     */
+    @Override
+    public int h2(Object key) {
+        return 1;
+    }
+}
