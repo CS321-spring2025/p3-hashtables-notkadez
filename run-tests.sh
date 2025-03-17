fc89e37e258ef9e3e67010b7e264eca3f84b6057
Kayden Humphries <kaydenhump4905@gmail.com>
2025-03-17 16:08:14 -0600

    Fixed run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -1,12 +1,42 @@
 #!/bin/sh
 
-function header(){
+function header() {
 	output=$1
-	for i in {1..80} 
+	for i in {1..80}
 	do
 		echo -n "-" >> $output
 	done
 	echo >> $output
+}
+
+function runtests() {
+  debugLevel=1
+  load=$1
+  echo "Running java HashtableExperiment dataSource = 3 loadFactor = $load "
+  java HashtableExperiment 3 $load $debugLevel  >> /dev/null
+  dos2unix linear-dump.txt double-dump.txt  >& /dev/null
+
+  echo
+  diff -w -B  linear-dump.txt test-cases/word-list-$load-linear-dump.txt > diff-linear-$load.out
+  if test "$?" = 0
+  then
+  	echo "Test PASSED for linear probing and load = $load"
+  	/bin/rm -f diff-linear-$load.out
+  else
+  	echo "==> Test FAILED for linear probing load = $load!! "
+  	echo "       Check the file diff-linear-$load.out for differences"
+  fi
+
+  diff -w -B  double-dump.txt test-cases/word-list-$load-double-dump.txt > diff-double-$load.out
+  if test "$?" = 0
+  then
+  	echo "Test PASSED for double probing and load = $load"
+  	/bin/rm -f diff-double-$load.out
+  else
+  	echo "==> Test FAILED for double probing load = $load!! "
+  	echo "       Check the file diff-double-$load.out for differences"
+  fi
+  echo
 }
 
 echo
@@ -30,32 +60,9 @@ echo
 
 
 dos2unix test-cases/* >& /dev/null
-debugLevel=1
-load=0.5
 
-echo "Running java HashtableExperiment dataSource = 3 loadFactor = $load "
-java HashtableExperiment 3 $load $debugLevel  >> /dev/null
-dos2unix linear-dump.txt double-dump.txt  >& /dev/null
-
-echo
-diff -w -B  linear-dump.txt test-cases/word-list-$load-linear-dump.txt > diff-linear-$load.out
-if test "$?" = 0
-then
-	echo "Test PASSED for linear probing and load = $load"
-	/bin/rm -f diff-linear-$load.out
-else
-	echo "==> Test FAILED for linear probing load = $load!! "
-	echo "       Check the file diff-linear-$load.out for differences"
-fi
-
-diff -w -B  double-dump.txt test-cases/word-list-$load-double-dump.txt > diff-double-$load.out
-if test "$?" = 0
-then
-	echo "Test PASSED for double probing and load = $load"
-	/bin/rm -f diff-double-$load.out
-else
-	echo "==> Test FAILED for double probing load = $load!! "
-	echo "       Check the file diff-double-$load.out for differences"
-fi
-echo
+for load in 0.5 0.6 0.7 0.8 0.9 0.95 0.99
+do
+  runtests $load
+done
 
